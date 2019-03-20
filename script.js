5aa0ae1eb632c67d69cc0974160c6bb2469f8315
zsavich <zsavich59@gmail.com>
2019-03-20 22:39:05 +0200

    Добавил задание task_2.5

--- script.js
+++ script.js
@@ -2,12 +2,26 @@ var ReferenceItem = /** @class */ (function () {
     function ReferenceItem(newTitle, newYear) {
         console.log('Creatiang a new ReferenceItem ...');
         this.title = newTitle;
-        this.year = newYear;
+        this._year = newYear;
     }
+    Object.defineProperty(ReferenceItem.prototype, "publisher", {
+        get: function () {
+            return this._publisher.toUpperCase();
+        },
+        set: function (newPublisher) {
+            this._publisher = newPublisher;
+        },
+        enumerable: true,
+        configurable: true
+    });
     ReferenceItem.prototype.printItem = function () {
-        console.log(this.title + " was published in " + this.year);
+        console.log("" + department);
     };
     return ReferenceItem;
 }());
 var ref = new ReferenceItem("Avengers: End Game", 2019);
+ref.printItem();
+ref.publisher = "David";
+console.log(ref.publisher);
+var department = "Mahuchkala";
 ref.printItem();
--- script.ts
+++ script.ts
@@ -1,17 +1,42 @@
 class ReferenceItem {
-    title: string;
-    year: number;
+
+    /*title: string;
+    year: number;*/
+
+    /*constructor(newTitle: string, newYear: number) {
+        console.log('Creatiang a new ReferenceItem ...');
+        this.title = newTitle;
+        this.year = newYear;
+    }*/
+
+    public title: string;
+    private _year: number;
+    private _publisher: string;
 
     constructor(newTitle: string, newYear: number) {
         console.log('Creatiang a new ReferenceItem ...');
         this.title = newTitle;
-        this.year = newYear;
+        this._year = newYear;
+    }
+
+    set publisher(newPublisher: string) {
+        this._publisher = newPublisher;
+    }
+
+    get publisher() {
+        return this._publisher.toUpperCase();
     }
 
     printItem(): void {
-        console.log(`${this.title} was published in ${this.year}`);
+        console.log(`${department}`);
     }
 }
 
 const ref: ReferenceItem = new ReferenceItem("Avengers: End Game", 2019);
+
+ref.printItem();
+ref.publisher = "David";
+console.log(ref.publisher);
+
+static const department = "Mahuchkala";
 ref.printItem();

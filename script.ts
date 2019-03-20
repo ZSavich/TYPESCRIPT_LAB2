43a760b68325178d6060d3cbe651fd35573e5bc5
zsavich <zsavich59@gmail.com>
2019-03-20 21:55:31 +0200

    Добавил задание 2.3

--- script.ts
+++ script.ts
@@ -1,40 +1,29 @@
-// Задание 1.1 Базовые типы
+// Задание 2.3 Расширение интерфейсов
 
-function getAllWorkers(): Worker {
-    return [
-        { id: 1, name: 'Ivan', surname: 'Ivanov', available: true, salary: Category.Designer},
-        { id: 2, name: 'Petro', surname: 'Petrov', available: true, salary: Category.Designer},
-        { id: 3, name: 'Vasyl', surname: 'Vasyliev', available: false, salary: Category.Designer},
-        { id: 4, name: 'Evgen', surname: 'Zhukov', available: true, salary: Category.Designer}
-    ];
-};
-
-// Задание 2.1 Определение интерфейсов
-
-interface Worker {
-    id: number;
+interface Person {
     name: string;
-    surname: string;
-    available: boolean;
-    salary: Category;   
+    email: string;
 }
 
-console.log(getAllWorkers());
-
-function getBookByID(worker: Worker) {
-    
+interface Author extends Person {
+    numBooksPublished: number;
 }
 
-function PrintWorker(worker: Worker): string {
-    return `${worker.name} ${worker.surname} got salary ${worker.salary}`;
+interface Librarian extends Person {
+    department: string;
+    assistCustomer(custName: string): void;
 }
 
-const igor: Worker = {
-    id: 1,
-    name: 'Ivan',
-    surname: 'Ivanov',
-    available: true,
-    salary: Category.Designer
-}
-
-console.log(PrintWorker(igor))
+const favoriteAuthor: Author = {
+    name: "Robert",
+    email: "robert@gmail.com",
+    numBooksPublished: 13
+};
+const favoriteLibrarian: Librarian = {
+    name: "John",
+    email: "john@hotmail.com",
+    department: "sience",
+    assistCustomer: function (custName: string): string {
+        return this.name + " " + custName;
+    }
+};

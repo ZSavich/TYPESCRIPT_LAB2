02dbb9201101f7e0ed22fb1495c7a7e4cb18d329
zsavich <zsavich59@gmail.com>
2019-03-20 21:36:32 +0200

    Добавил задание 2.1

--- script.ts
+++ script.ts
@@ -1,32 +1,27 @@
-// Задание 1.1 Базовые типы
-
-function getAllWorkers(): Worker {
-    return [
-        { id: 1, name: 'Ivan', surname: 'Ivanov', available: true, salary: Category.Designer},
-        { id: 2, name: 'Petro', surname: 'Petrov', available: true, salary: Category.Designer},
-        { id: 3, name: 'Vasyl', surname: 'Vasyliev', available: false, salary: Category.Designer},
-        { id: 4, name: 'Evgen', surname: 'Zhukov', available: true, salary: Category.Designer}
-    ];
-};
-
-// Задание 2.1 Определение интерфейсов
+/ Задание 2.1 Определение интерфейсов
 
 interface Worker {
     id: number;
     name: string;
     surname: string;
     available: boolean;
-    salary: Category;   
+    salary: Category;
 }
 
-console.log(getAllWorkers());
+const enum Category {
+    BusinessAnalyst,
+    Developer,
+    Designer,
+    QA,
+    ScrumMaster
+}
 
 function getBookByID(worker: Worker) {
-    
+
 }
 
 function PrintWorker(worker: Worker): string {
-    return `${worker.name} ${worker.surname} got salary ${worker.salary}`;
+    return worker.name + " " + worker.surname + " got salary " + worker.salary;
 }
 
 const igor: Worker = {
@@ -37,4 +32,4 @@ const igor: Worker = {
     salary: Category.Designer
 }
 
-console.log(PrintWorker(igor))
+console.log(PrintWorker(igor));
